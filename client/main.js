461c29b108d90f0e64884d6df0ebe4e0b21bc91e
anhyoseung <ace.aaiden@gmail.com>
2022-12-13 18:33:13 +0900

    에이든

--- client/main.js
+++ client/main.js
@@ -7,11 +7,11 @@ import "/imports/routes";
 import "bootstrap";
 
 Meteor.startup(function () {
-  // Meteor.setTimeout(callAI, 5000)
+  // Meteor.setTimeout(callAI, 5000);
 });
 
 function callAI() {
-  const next = Math.floor(Madth.random() * 3000) + 7000;
+  const next = Math.floor(Math.random() * 3000) + 7000;
 
   Meteor.userId() && Meteor.call("_ai");
   Meteor.setTimeout(callAI, next);
--- imports/server/methods/RoomInsert.js
+++ imports/server/methods/RoomInsert.js
@@ -1,35 +1,19 @@
 import { Meteor } from "meteor/meteor";
 import { Rooms, Read } from "/imports/collections";
 
-// "rooms": {
-//   "_id": "String",
-//     "updatedAt": "Date",
-//     "lastUserId": "String",
-//     "lastUserName": "String",
-//     "lastUserAvatar": "String",
-//     "lastMessage": "String",
-//     "joiner": "Array"
-// },
-//
-// "read": {
-//   "_id": "String",
-//     "readAt": "Date",
-//     "roomId": "String",
-//     "userId": "String"
-// },
 Meteor.methods({
   roomInsert() {
     const time = new Date(); //비교??
 
     Rooms.insert({
-      updateAt: time,
+      updatedAt: time,
       lastUserId: "",
       lastUserName: "new chat room",
       lastUserAvatar: "",
       lastMessage: "새로운 채팅방이 생성되었습니다",
       joiner: [this.userId], //방 참여자 아이디 추가될 것이므로 ID이다
     });
-    const room_id = Rooms.findOne({ updateAt: time })._id; //room_id
+    const room_id = Rooms.findOne({ updatedAt: time })._id; //room_id
 
     Read.insert({
       readAt: time,
